30930a7cae21b9b30e3cde61b6b4dc2e6a5c53d3
MatiasRojas93 <141685523+MatiasRojas93@users.noreply.github.com>
2023-10-03 18:56:55 -0300

    pacientedata y conection
    p y c

---
+++ src/nutricorp/AccesoADatos/CConection.java
@@ -0,0 +1,27 @@
+package nutricorp.AccesoADatos;
+import java.sql.*;
+import javax.swing.JOptionPane;
+public class CConection {
+    private static final String URL="jdbc:mariadb://localhost/";
+    private static final String DB="universidadulp";
+    private static final String USUARIO="root";
+    private static final String PASSWORD="";
+    private static Connection connection;
+            
+         private CConection(){}
+         
+         public static Connection getConexion(){
+             if(connection==null){
+                 try{
+                     Class.forName("org.mariadb.jdbc.Driver");
+                     connection=DriverManager.getConnection(URL+DB,USUARIO,PASSWORD);
+                 }catch(ClassNotFoundException ex){
+                    JOptionPane.showMessageDialog(null, "Error al cargar los driver "+ex);
+             }   catch (SQLException ex) {
+                     JOptionPane.showMessageDialog(null,"Error al conectarse a la base de datos"+ ex);
+             }
+             }
+             return connection;
+         }
+            
+}
--- src/nutricorp/AccesoADatos/PacienteData.java
+++ src/nutricorp/AccesoADatos/PacienteData.java
@@ -1,14 +1,47 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *nombre , dni , domicilio , telefono , idpaciente
  */
 package nutricorp.AccesoADatos;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import nutricorp.Entidades.Paciente;
 /**
  *
  * @author Matias
  */
-public class PacienteData {
+public class PacienteData extends Paciente{
+    Connection connection;
+    PreparedStatement ps;
+    ResultSet rs;
+    String sql = "";
     
+    public void insertarPaciente(Paciente paciente) {
+        try {
+            // Establecer la conexión antes de la preparación de la sentencia SQL
+            connection = CConection.getConexion();
+
+            sql = "INSERT INTO `alumno`( `nombre`, `dni`, `domicilio`, `telefono`) VALUES (?,?,?,?);";
+            ps = connection.prepareStatement(sql);
+
+            // Completar los campos con símbolos por las variables
+            // Primera posición corresponde al primer símbolo y así sucesivamente, la contabilización arranca desde 1
+            ps.setString(1, paciente.getNombre());
+            ps.setInt(2, paciente.getDni());
+            ps.setString(3, paciente.getDominicilio());
+            ps.setString(4, paciente.getTelefono());    
+            ps.executeUpdate();
+            // Cerrar la consulta y otros recursos si es necesario
+            System.out.println("Paciente cargado correctamente ");
+            JOptionPane.showMessageDialog(null, "Paciente Ingresado Correctamente");
+
+        } catch (SQLException | NumberFormatException e) { // Coloca esto aquí
+            // Coloca esto aquí
+            System.out.println("Error al cargar los datos del Pacientealumno: " + e.getMessage());
+        }
+    }
 }
