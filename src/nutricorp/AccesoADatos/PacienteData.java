344f838c301a19f33068d4ab409eea942496e934
MatiasRojas93 <141685523+MatiasRojas93@users.noreply.github.com>
2023-10-05 18:45:11 -0300

    com
    com

--- src/nutricorp/AccesoADatos/PacienteData.java
+++ src/nutricorp/AccesoADatos/PacienteData.java
@@ -4,14 +4,11 @@
 package nutricorp.AccesoADatos;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import nutricorp.Entidades.Paciente;
 /**
